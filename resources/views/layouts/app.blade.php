2424cede9f83bf8e88530a6ca0de76a81bcbcff9
Erliandikasyahputraa <syahputraerliandika@gmail.com>
2026-03-13 10:18:47 +0700

    Fix: Added missing separator in tab titles and hardened layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ $title ?? (View::hasSection('title') ? View::getSection('title') . ' - ' : '') }}{{ config('app.name', 'BINGO') }}</title>
+    <title>@if(isset($title)) {{ $title }} - @elseif(View::hasSection('title')) @yield('title') - @endif {{ config('app.name', 'BINGO') }}</title>
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700,900" rel="stylesheet" />
